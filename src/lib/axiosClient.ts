c7795d378382304fd48205af1181c0dd75a3fb5a
goayasushi <goayasushi@gmail.com>
2025-01-18 16:41:00 +0900

    axios client

---
+++ src/lib/axiosClient.ts
@@ -0,0 +1,36 @@
+"use client";
+
+import axios, { AxiosInstance } from "axios";
+
+// create axios instance
+const axiosClient: AxiosInstance = axios.create({
+  baseURL: "http://localhost:8000",
+});
+
+// request interceptor
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token && config.headers) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// response interceptor
+axiosClient.interceptors.response.use(
+  (response) => {
+    const newToken = response.headers["x-access-token"];
+    if (newToken) {
+      localStorage.setItem("access_token", newToken);
+    }
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosClient;
