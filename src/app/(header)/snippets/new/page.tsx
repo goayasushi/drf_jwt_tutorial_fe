89a92d1431f431cac8a72ba0c5bc20c36aedd9e3
goayasushi <goayasushi@gmail.com>
2025-02-06 15:50:21 +0900

    snippet 投稿ページ フォームの共通化

--- src/app/(header)/snippets/new/page.tsx
+++ src/app/(header)/snippets/new/page.tsx
@@ -1,43 +1,15 @@
 "use client";
 
-import { Box, Fieldset, Flex, Input, Text, Textarea } from "@chakra-ui/react";
-import { Field } from "@/components/ui/field";
-import {
-  NativeSelectField,
-  NativeSelectRoot,
-} from "@/components/ui/native-select";
-import { Button } from "@/components/ui/button";
-import { Switch } from "@/components/ui/switch";
-import { Controller, useForm } from "react-hook-form";
-import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
-import { dracula } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { Box } from "@chakra-ui/react";
+
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
 import { SnippetFormData } from "@/types/snippetFormData";
 import axiosClient from "@/lib/axiosClient";
-import { LANGUAGES_HLJS } from "@/config/availabe_languages_hljs";
+import { SnippetForm } from "@/components/SnippetForm";
 
 export default function SnippetNew() {
-  // formの設定
-  const {
-    register,
-    handleSubmit,
-    control,
-    watch,
-    reset,
-    formState: { errors },
-  } = useForm<SnippetFormData>({
-    defaultValues: {
-      code: "",
-      language: "",
-      linenos: false,
-    },
-  });
-
-  // form入力値のリアルタイム監視
-  const { code, language, linenos } = watch();
-
   const router = useRouter();
   const queryClient = useQueryClient();
 
@@ -46,126 +18,22 @@ export default function SnippetNew() {
     return data;
   };
 
-  const mutation = useMutation({
+  const createSnippet = useMutation({
     mutationFn: fetchCreateSnippet,
     onSuccess: () => {
       // スニペット一覧のキャッシュを無効化
       queryClient.invalidateQueries({ queryKey: ["snippets"] });
 
-      // formリセット
-      reset();
-
-      // 一覧画面に遷移
       router.push("/snippets");
     },
   });
 
-  const createSnippet = async (newSnippet: SnippetFormData) => {
-    mutation.mutate(newSnippet);
-  };
-
   return (
     <Box p="6">
-      <form onSubmit={handleSubmit(createSnippet)}>
-        <Flex justify="center" h="100%">
-          <Fieldset.Root
-            size="lg"
-            w="100%"
-            p={6}
-            border="1px solid"
-            borderColor="gray.200"
-            borderRadius="md"
-            boxShadow="md"
-            bg="white"
-          >
-            <Fieldset.Content>
-              <Field invalid={!!errors.title} errorText={errors.title?.message}>
-                <Input
-                  {...register("title", {
-                    required: "タイトルは必須です",
-                  })}
-                  placeholder="タイトルを入力してください"
-                />
-              </Field>
-              <Field
-                w="300px"
-                label="言語"
-                invalid={!!errors.language}
-                errorText={errors.language?.message}
-              >
-                <NativeSelectRoot>
-                  <NativeSelectField
-                    {...register("language", {
-                      required: "言語は必須です",
-                    })}
-                    placeholder="言語を選択してください"
-                    items={LANGUAGES_HLJS}
-                  />
-                </NativeSelectRoot>
-              </Field>
-              <Controller
-                name="linenos"
-                control={control}
-                render={({ field }) => (
-                  <Field>
-                    <Switch
-                      name={field.name}
-                      checked={field.value}
-                      onCheckedChange={({ checked }) => field.onChange(checked)}
-                      inputProps={{ onBlur: field.onBlur }}
-                    >
-                      行数の表示
-                    </Switch>
-                  </Field>
-                )}
-              />
-
-              <Field
-                label="本文"
-                invalid={!!errors.code}
-                errorText={errors.code?.message}
-              >
-                <Flex w="100%" h="calc(100vh - 500px)">
-                  <Textarea
-                    flex="1"
-                    h="100%"
-                    p="20px"
-                    resize="none"
-                    placeholder="スニペットを書いて投稿しよう"
-                    {...register("code", {
-                      required: "スペニットの記載は必須です",
-                    })}
-                  />
-                  <Box flex="1" h="100%">
-                    <SyntaxHighlighter
-                      language={language}
-                      style={dracula}
-                      showLineNumbers={linenos}
-                      customStyle={{
-                        height: "100%",
-                        padding: "20px",
-                        borderRadius: "4px",
-                      }}
-                    >
-                      {code}
-                    </SyntaxHighlighter>
-                  </Box>
-                </Flex>
-              </Field>
-            </Fieldset.Content>
-            <Button
-              loading={mutation.isPending}
-              type="submit"
-              alignSelf="flex-start"
-            >
-              投稿
-            </Button>
-            {mutation.isError ? (
-              <Text>エラーが発生しました: {mutation.error.message}</Text>
-            ) : null}
-          </Fieldset.Root>
-        </Flex>
-      </form>
+      <SnippetForm
+        onSubmitFn={(newSnippet) => createSnippet.mutate(newSnippet)}
+        isSubmitting={createSnippet.isPending}
+      />
     </Box>
   );
 }
